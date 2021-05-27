d6a3b8a5d0aa13a1bb736009e8c6d53691d7d596
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-05-27 23:16:44 +0200

    add _form erb file on views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,24 +2,38 @@
 
 class UsersController < ApplicationController
   def new
-        @user = User.new
+    @user = User.new(name: 'Tanzila', email: 'tanzilaabedin01@gmail.com')
   end
 
   def create
-     #  @user = User.new(name: params[:username], email: params[:email], password: params[:password])
+    #  @user = User.new(name: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
     if @user.save
       redirect_to @user
     else
-      render :new
+      render :show
     end
+  end
+
+  def index
+    @users = User.all
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
- private
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find_by(params[:id])
+    render 'show' if @user.update(user_params)
+  end
+
+  private
+
   def user_params
     params.require(:user).permit(:name, :email, :password)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # post  '/users',  to: 'users#create'
-  resources :users, only: %i[new create]
+  resources :users
   # root 'application#hello'
 end
