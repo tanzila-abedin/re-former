3cf9abe11ac1323ef6efb3ffe38b54bb47598185
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-05-27 17:47:32 +0200

    create Railsy-er Forms with #form_with

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@
 
 class UsersController < ApplicationController
   def new
-    #     @user = User.new(name: 'Tanz', email: 'tanzila.abedin@yahoo.com')
+        @user = User.new
   end
 
   def create
-       @user = User.new(name: params[:username], email: params[:email], password: params[:password])
-#     @user = User.new(user_params)
+     #  @user = User.new(name: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
     if @user.save
       redirect_to @user
     else
@@ -15,8 +15,8 @@ class UsersController < ApplicationController
     end
   end
   
-#  private
-#   def user_params
-#     params.require(:user).permit(:name, :email, :password)
-#   end
+ private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
